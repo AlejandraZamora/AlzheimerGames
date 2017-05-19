4b37729bcdf9290386b9abb8f269de146d4f8571
AlejandraZamora <alejandra26.az@gmail.com>
2017-05-19 11:49:32 -0500

    Client

--- target/classes/static/app/services/services.js
+++ target/classes/static/app/services/services.js
@@ -2,10 +2,10 @@
 
 angular.module('services.factory', ['ngRoute', 'ngResource'])
 .factory('persona', function($resource){
-	return $resource('https://alzheimergameservices.herokuapp.com/persona/:personaId',{id:"@_personaId"},{get: { method: 'GET'}});
+	return $resource('https://alzheimergameservices.herokuapp.com/persona/:personaId',{id:"@_personaId"},{get: { method: 'GET'}, save: {method: 'POST',headers: { 'Content-Type': 'application/json' }}});
 })
 .factory('personas', function($resource) {
-	return $resource('https://alzheimergameservices.herokuapp.com/persona',{},{ 'get': { method: 'GET', isArray: true}, 'update': { method: 'PUT', isArray: false}});
+	return $resource('https://alzheimergameservices.herokuapp.com/persona',{},{ 'get': { method: 'GET', isArray: true}, 'update': { method: 'PUT', headers: { 'Content-Type': 'application/json'}, isArray: false}});
 })
 .factory('newPersona', function($resource) {
 	return $resource('https://alzheimergameservices.herokuapp.com/persona/new');
